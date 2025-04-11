03080566fc721141439c69749024da508ad4d773
xVilho <65539818+xVilho@users.noreply.github.com>
2025-04-11 19:01:20 +0300

    initial release

---
+++ longerdays.cs
@@ -0,0 +1,21 @@
+﻿using MelonLoader;
+using Il2CppScheduleOne.GameTime;
+using UnityEngine;
+
+[assembly: MelonInfo(typeof(LongerDaysMod), "LongerDays", "1.0.0", "xVilho")]
+
+public class LongerDaysMod : MelonMod
+{
+    public override void OnSceneWasInitialized(int buildIndex, string sceneName)
+    {
+        TimeManager timeManager = Object.FindObjectOfType<TimeManager>();
+        if (timeManager == null)
+        {
+            MelonLogger.Warning("TimeManager not found.");
+            return;
+        }
+
+        timeManager.TimeProgressionMultiplier = 0.5f;
+        MelonLogger.Msg("TimeProgressionMultiplier set to 0.5 (2x longer days)");
+    }
+}
